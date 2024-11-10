88795dcbcc7dab0674ad3d77ff08d7aece31c5c3
Tom Haddad <thomas.haddad@ivy-solutions.de>
2024-11-10 15:35:28 +0100

    init convex

---
+++ README.md
@@ -0,0 +1,20 @@
+# React Native / Convex Demo
+
+## Get started
+
+1. Install dependencies
+
+   ```bash
+   npm install
+   ```
+2. Start Convex
+
+   ```bash
+   npx convex dev
+   ```
+
+3. Start the app
+
+   ```bash
+    npx expo start
+   ```
---
+++ convex/auth.ts
@@ -0,0 +1,6 @@
+import { convexAuth } from "@convex-dev/auth/server";
+import Google from "@auth/core/providers/google";
+
+export const { auth, signIn, signOut, store } = convexAuth({
+  providers: [Google({ clientId: process.env.GOOGLE_CLIENT_ID! })],
+});
